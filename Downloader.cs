d1e2e823f758fc1b89f0d7375bb769ad54f4db89
oselish <55660821+oselish@users.noreply.github.com>
2023-09-23 16:58:27 +0500

    Add files via upload

---
+++ Downloader.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace DownloadFromM3U8
+{
+	public delegate void DownloadFinished();
+
+	internal class Downloader
+	{
+		public string url { get; private set; }
+		public string ffmpegPath { get; private set; }
+		public string downloadPath { get; private set; }
+
+		public string m3u8 {  get; private set; }
+
+		public DownloadFinished downloadFinished;
+
+
+		public Downloader(string url)
+		{
+			this.url = url;
+			WebRequest webRequest = HttpWebRequest.Create(url);
+			webRequest.Method = "HEAD";
+			webRequest.GetResponse();
+
+			ffmpegPath = Path.Combine(Environment.CurrentDirectory, "ffmpeg-master-latest-win64-gpl\\bin\\ffmpeg.exe");
+
+			if (!File.Exists(ffmpegPath))
+			{
+				throw new Exception("ffmpeg.exe is not found");
+			}
+
+			m3u8 = GetM3U8FromHtml(url);
+			if (m3u8 == null)
+			{
+				throw new Exception("Link to the .m3u8-playlist is not found on this page");
+			}
+		}
+
+
+		/// <summary>
+		/// Start downloading video to the folder
+		/// </summary>
+		/// <param name="downloadPath"></param>
+		public async void StartDownloading(string downloadPath)
+		{
+			string arguments = $"-i \"{m3u8}\" -c copy -bsf:a aac_adtstoasc \"{downloadPath}\"";
+			string program = $"\"{ffmpegPath}\"";
+
+			var ffmpegProcessInfo = new ProcessStartInfo()
+			{
+				FileName = program,
+				Arguments = arguments,
+				UseShellExecute = false,
+				CreateNoWindow = true,
+			};
+
+			Process ffmpegProcess = new Process()
+			{
+				StartInfo = ffmpegProcessInfo,
+				EnableRaisingEvents = true
+			};
+
+			await Task.Run(() =>
+			{
+				ffmpegProcess.Start();
+				ffmpegProcess.WaitForExit();
+			});
+			downloadFinished.Invoke();
+		}
+
+
+		/// <summary>
+		/// Getting link to the .m3u8 file from HTML page
+		/// </summary>
+		/// <param name="url"></param>
+		/// <returns></returns>
+		private string GetM3U8FromHtml(string url)
+		{
+			HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+			req.Method = "GET";
+			req.CookieContainer = new CookieContainer();
+			req.AllowAutoRedirect = true;
+
+			string m3u8Dirty = "";
+
+			HttpWebResponse Response = (HttpWebResponse)req.GetResponse();
+			StreamReader sr = new StreamReader(Response.GetResponseStream());
+			string htmlcode = sr.ReadToEnd();
+
+			foreach (var line in htmlcode.Split('\n'))
+				if (line.Contains("m3u8"))
+				{
+					m3u8Dirty = line;
+					break;
+				}
+
+			if (m3u8Dirty == null)
+				return null;
+
+			string m3u8Clean = "";
+
+			bool startParse = false;
+			
+
+			for (int i = 0; i < m3u8Dirty.Length; i++)
+			{
+				if (m3u8Dirty.Substring(i, 5) == "https") 
+					startParse = true;
+				if (startParse)
+				{
+					if (m3u8Dirty.Substring(i, 5) == ".m3u8")
+					{
+						m3u8Clean += ".m3u8";
+						break;
+					}
+					m3u8Clean += m3u8Dirty[i];
+				}
+			}
+
+			return m3u8Clean;
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace DownloadFromM3U8
+{
+	public partial class MainWindow : Window
+	{
+		Downloader downloader;
+		SaveFileDialog saveFileDialog;
+
+		delegate void DownloadFinished();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			saveFileDialog = new SaveFileDialog();
+			saveFileDialog.Title = "Save file";
+			saveFileDialog.DefaultExt = ".mp4";
+			saveFileDialog.Filter = $"Video in format MP4 | *{saveFileDialog.DefaultExt}";
+
+			OpenFileButton.IsEnabled = false;
+			OpenFolderButton.IsEnabled = false;
+
+			string path = "";
+
+			DownloadButton.Click += (s, e) =>
+			{
+				try
+				{
+					saveFileDialog.FileName = "output";
+					downloader = new Downloader(UrlTextBox.Text);
+					
+					var showDialogResult = saveFileDialog.ShowDialog();
+					
+					downloader.downloadFinished += Downloaded;
+
+					if (saveFileDialog.OverwritePrompt)
+						File.Delete(saveFileDialog.FileName);
+
+					path = Directory.GetParent(saveFileDialog.FileName).FullName;
+					VideoSavePath.Text = $"Downloading {saveFileDialog.FileName}";
+
+					if (showDialogResult == false)
+						throw new Exception("Choose the folder for file");
+
+					downloader.StartDownloading(saveFileDialog.FileName);
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+				}
+			};
+			OpenFileButton.Click += (s, e) => Process.Start(saveFileDialog.FileName);
+			OpenFolderButton.Click += (s, e) => Process.Start("explorer.exe", path);
+		}
+
+		public void Downloaded()
+		{
+			OpenFolderButton.IsEnabled = true;
+			OpenFileButton.IsEnabled = true;
+			var fileName = Path.GetFileName(saveFileDialog.FileName);
+			VideoSavePath.Text = $"The file \"{fileName}\" was succesfully downloaded";
+			MessageBox.Show($"The file \"{saveFileDialog.FileName}\" was succesfully downloaded", "Success", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+		}
+	}
+}
